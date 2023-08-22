49b3ef9ada6a136dbad8442889eeca35cdf5f525
Shubham Ramesh Mane <136062065+shubmane-embedded@users.noreply.github.com>
2023-08-22 20:29:38 +0530

    Add files via upload

---
+++ Micro_python/Deep_sleep/deep_sleep.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2023
+# All rights reserved.
+#
+# Auther : Shubham Mane
+# Date Created : 15 August 2023
+# Date Modified : 15 August 2023
+#
+# @Description : Demonstration of the Deep sleep on ESP32
+# @file : deep_sleep.py
+
+import machine
+from machine import Pin
+from time import sleep
+
+print("----Demonstration of Deep Sleep mode----\n")
+
+led_pin = Pin(2, Pin.OUT)
+
+def led_blink():
+    for i in range(5):
+        led_pin(1)
+        print("LED ON")
+        sleep(1)
+        led_pin(0)
+        print("LED OFF")
+        sleep(1)
+
+led_blink()
+
+print("Entering into deep sleep")
+machine.deepsleep(10000)
+print("done")
+
+# End of script
