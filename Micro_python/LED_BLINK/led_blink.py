275fcf99cbe443dc15d26250212bfdc3c036a50b
Shubham Ramesh Mane <136062065+shubmane-embedded@users.noreply.github.com>
2023-09-03 00:52:44 +0530

    Update led_blink.py

--- Micro_python/LED_BLINK/led_blink.py
+++ Micro_python/LED_BLINK/led_blink.py
@@ -17,24 +17,33 @@ print("----First ESP32 WROOM 32 with Micropython----\n")
 print("----LED Blink on GPIO 2 on ESP32 WROOM 32----\n")
 print("------------Blinking LED  7 times------------\n")
 
-#variable
+# Variable to count (ON & OFF) events
 count = 0
 
+# Configure GPIO 2 as OUTPUT
 led = Pin(2, Pin.OUT)
 
 while True:
+
+    # LED ON
     led.value(1)
     print("STATUS - LED ON")
     sleep(1)
+
+    # LED OFF
     led.value(0)
     print("STATUS - LED OFF")
     sleep(1)
+    
     # count increment
     count = count + 1
     print("count = {0}\n".format(count))
+    
     # checking for count
     if count == 7:
         print("Done!!! Exiting\n")
         break
+
+exit(0)
 
 # End of script
