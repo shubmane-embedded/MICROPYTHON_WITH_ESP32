239d050977c224e86460237d5a2ad351a3301466
Shubham Ramesh Mane <136062065+shubmane-embedded@users.noreply.github.com>
2024-02-10 11:15:41 +0530

    Update deep_sleep.py

--- Micro_python/Deep_sleep/deep_sleep.py
+++ Micro_python/Deep_sleep/deep_sleep.py
@@ -3,32 +3,32 @@
 # Copyright (c) 2023
 # All rights reserved.
 #
-# Auther : Shubham Mane
-# Date Created : 15 August 2023
-# Date Modified : 15 August 2023
+# Author: Shubham Mane
+# Date Created: 15 August 2023
+# Date Modified: 15 August 2023
 #
-# @Description : Demonstration of the Deep sleep on ESP32
-# @file : deep_sleep.py
+# Description: Demonstration of the Deep sleep on ESP32
+# File: deep_sleep.py
 
 import machine
 from machine import Pin
 from time import sleep
 
 # Demonstration of Deep Sleep mode
-print("----Demonstration of Deep Sleep mode----\n")
+print("---- Demonstration of Deep Sleep mode ----\n")
 
 # Configure the GPIO 2 as Output
 led_pin = Pin(2, Pin.OUT)
 
 # Function definition ==> led_blink
 def led_blink():
-    for i in range(5):
-        led_pin(1)
-        print("LED ON")
-        sleep(1)
-        led_pin(0)
-        print("LED OFF")
-        sleep(1)
+  for i in range(5):
+    led_pin(1)
+    print("LED ON")
+    sleep(1)
+    led_pin(0)
+    print("LED OFF")
+    sleep(1)
 
 # led blink function call
 led_blink()
